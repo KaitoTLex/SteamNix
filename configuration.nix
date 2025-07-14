bdb0bd7ec7fbe119f15dcefe3b13995234e2f490
Ren Lin <kaitotlex@MBPKanadeXNU.local>
2025-07-13 21:49:32 -0700

    feat: Nvidia GPU and NixOS 25.05

--- configuration.nix
+++ configuration.nix
@@ -37,6 +37,54 @@
   systemd.extraConfig = "DefaultTimeoutStopSec=5s";
 
   ################
+  #  Graphics    #
+  ################ 
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
+  hardware.nvidia = {
+    # custom option defined in graphics/default.nix
+    #usePatchedAquamarine = true;
+
+    # Modesetting is required.
+    modesetting.enable = lib.mkForce true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = lib.mkForce true;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = lib.mkForce true;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = lib.mkForce false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    #Power Saving Features
+    # prime = {
+    #   offload.enable = lib.mkForce true;
+    #   # Make sure to use the correct Bus ID values for your system!
+    #   #intelBusId = "PCI:";
+    #   nvidiaBusId = "PCI:1:0:0";
+    #   amdgpuBusId = "PCI:8:0:0";
+    # };
+
+  };
+ 
+  ################
   # FileSystems  #
   ################
   fileSystems."/" = {
@@ -153,9 +201,14 @@
 
   ######################
   ######################
-
+#   fileSystems."/run/media/steamos/HDD" = {
+#    device = "/dev/disk/by-uuid/c8c86bd3-eb06-4010-8309-5724bd18e381";
+#    fsType = "btrfs";
+#    options = [
+#      "users"  "nofail" "compress=zstd" "nosuid" "nodev" ];
+#  };
   ########################
   # System State Version #
   ########################
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.05;
 }
